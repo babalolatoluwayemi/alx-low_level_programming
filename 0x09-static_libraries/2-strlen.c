6b9bda26f3ab841f816d0dd6c4486c72ec8905f9
babalolatoluwayemi <babalolatoluwayemi@gmail.com>
2022-09-30 13:26:41 -0700

    libmy

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,27 +1,17 @@
 #include "main.h"
 
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
+ * _strlen - Return the length of a string.
+ * @s: string.
  *
- * Return: The integer value of the converted string.
+ * Return: Length.
  */
-int _atoi(char *s)
+int _strlen(char *s)
 {
-	int sign = 1;
-	unsigned int num = 0;
+	int len = 0;
 
-	do {
-		if (*s == '-')
-			sign *= -1;
+	while (*s++)
+		len++;
 
-		else if (*s >= '0' && *s <= '9')
-			num = (num * 10) + (*s - '0');
-
-		else if (num > 0)
-			break;
-
-	} while (*s++);
-
-	return (num * sign);
+	return (len);
 }
